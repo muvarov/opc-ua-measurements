411362fad617fa1f3331402095cdc88206a04523
Maxim Uvarov <maxim.uvarov@linaro.org>
2019-03-05 10:35:34 +0300

    gendata.py: fix data indexes
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- gendata.py
+++ gendata.py
@@ -6,8 +6,10 @@ print "openning file %s" % file
 
 f = open(file, "r") 
 for line in f:
-    enter =  float(line.split(" ")[4][:-1])
+    enter = line.split(" ")[6][:-1]
+    enter =  float(enter)
     nextline=f.next()
-    end= float(nextline.split(" ")[4][:-1])
+    end = nextline.split(" ")[6][:-1]
+    end= float(end)
     print "%.6f" % (end - enter)
 f.close()
